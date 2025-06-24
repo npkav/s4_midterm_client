16b387115fcb3d5e286a367591bcc2ee9491d184
Nick <174647283+npkav@users.noreply.github.com>
2025-06-24 16:27:50 -0230

    updated customer model constructors

--- src/main/java/org/cli/model/Customer.java
+++ src/main/java/org/cli/model/Customer.java
@@ -1,24 +1,31 @@
-// sample for Customer class before database is implemented
-
 package org.cli.model;
 
 public class Customer {
-    private int id;
+    private Long id;
     private String firstName;
     private String lastName;
     private String email;
 
-    public Customer(int id, String firstName, String lastName, String email) {
+    public Customer() {}
+
+    public Customer(Long id, String firstName, String lastName, String email) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
     }
 
-    public int getId() { return id; }
+    public Long getID() { return id; }
+    public void setID(Long id) { this.id = id; }
+
     public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+
     public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+
     public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
 
     @Override
     public String toString() {
